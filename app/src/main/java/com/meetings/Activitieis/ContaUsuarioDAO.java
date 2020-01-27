711d746c6f26853f3900c1ec1bf60d9be62b2c29
Lucas <lucasaugustodelaraartigas@gmail.com>
2020-01-27 03:57:42 -0300

    Melhorando login

--- app/src/main/java/com/meetings/Activitieis/ContaUsuarioDAO.java
+++
@@ -1,6 +0,0 @@
-package com.meetings.Activitieis;
-
-class ContaUsuarioDAO {
-    public void salva(ContaUsuario contaUsuario) {
-    }
-}
--- app/src/main/java/com/meetings/Activitieis/MainActivity.java
+++ app/src/main/java/com/meetings/Activitieis/MainActivity.java
@@ -1,30 +1,83 @@
 package com.meetings.Activitieis;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.meetings.R;
+
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.meetings.ListaSalasAdapter;
-import com.meetings.R;
+
 
 public class MainActivity extends AppCompatActivity {
+    private EditText campoEmail;
+    private EditText campoSenha;
+    private ContaUsuario contaUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("Salas da Wise");
-        vaiTelaLogin();
+        setContentView(R.layout.activity_login);
+        setTitle("Meetings");
+
+        inicializacaoDosCampos();
+
+        Button botaoLogin = findViewById(R.id.login);
+        //Button botaoCadastrar = findViewById(R.id.cadastrar);
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+    private void inicializacaoDosCampos() {
+        campoEmail = findViewById(R.id.email);
+        campoSenha = findViewById(R.id.senha);
+    }
+
+
+    private void preencheCadastro() {
+        String email = campoEmail.getText().toString();
+        String senha = campoSenha.getText().toString();
+
+        if (contaUsuario != null) {
+            contaUsuario.setEmail(email);
+            contaUsuario.setSenha(senha);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.login){
+            finalizaLogin();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void finalizaLogin() {
 
     }
 
-    private void vaiTelaLogin() {
+    private void preencheCampos() {
+        campoEmail.setText(contaUsuario.getEmail());
+        campoSenha.setText(contaUsuario.getSenha());
     }
+
+
 }
 
